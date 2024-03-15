d0fc49b7779ca199f5cd5801d9954db3ab5926b8
marcelamejiao <mejiao24@hotmail.com>
2024-03-15 15:58:10 +1100

    implement query to obtain the amount of sales per employee for each month in 2021

--- query.sql
+++ query.sql
@@ -102,6 +102,57 @@ GROUP BY soldYear
 ORDER BY soldYear
 
 
+-- 8. Create a report that shows the amount of sales per employee for each month in 2021
+
+
+SELECT emp.lastName, emp.firstName,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '01'
+        THEN salesAmount END) AS JanSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '02'
+        THEN salesAmount END) AS FebSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '03'
+        THEN salesAmount END) AS MarSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '04'
+        THEN salesAmount END) AS AprSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '05'
+        THEN salesAmount END) AS MaySales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '06'
+        THEN salesAmount END) AS JunSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '07'
+        THEN salesAmount END) AS JulSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '08'
+        THEN salesAmount END) AS AugSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '09'
+        THEN salesAmount END) AS SepSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '10'
+        THEN salesAmount END) AS OctSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '11'
+        THEN salesAmount END) AS NovSales,
+  SUM(CASE 
+        WHEN strftime('%m', soldDate) = '12'
+        THEN salesAmount END) AS DecSales
+FROM sales sal
+INNER JOIN employee emp 
+  ON sal.employeeId = emp.employeeId
+WHERE sal.soldDate 
+  BETWEEN '2021-01-01' AND '2021-12-31'
+GROUP BY emp.lastName, emp.firstName
+ORDER BY emp.lastName, emp.firstName
+
+
+
+
 
 
 
